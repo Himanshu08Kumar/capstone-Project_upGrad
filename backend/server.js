d4a3b0037c8e96a6c6bec3b472ef0b6e22b2dead
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-11-21 15:15:08 +0530

    doctor's schema is ready

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+//app config
+const app = express();
+const port = process.env.PORT || 3000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//api endpoints
+
+app.get('/',(req, res) =>{
+    res.send("Api working")
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
