8ef3cd312825b88ff059b9227482904428cb713c
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-19 22:29:10 +0530

    Implemented the doctor's appointment feature

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { assets } from '../assets/assets_admin/assets'
+import Healthcare from "../../../frontend/public/icon/HealthCare.png"
 import { AdminContext } from '../context/AdminContext'
 import { useNavigate } from "react-router-dom"
 
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
         <div className=' flex items-center gap-2 text-xs'>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+            <img className='w-36 sm:w-40 cursor-pointer' src={Healthcare} alt="" />
             <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
       <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
