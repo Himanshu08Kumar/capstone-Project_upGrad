0aab681db1c96c4f26d5bf590c5467f7bcb7cdbb
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-19 11:17:32 +0530

    shows all doctors on admin side

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -72,5 +72,17 @@ const adminLogin = async (req, res) => {
         res.json({sucess: false, message:error.message})
     }
 }
+//API to get all doctors list for admin panel
 
-export {addDoctor, adminLogin}
+const allDoctors = async (req, res) =>{
+    try {
+        const doctors = await doctorModel.find({}).select('-password')
+        res.json({success:true, doctors})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({sucess: false, message:error.message})
+    }
+}
+
+export {addDoctor, adminLogin, allDoctors}
