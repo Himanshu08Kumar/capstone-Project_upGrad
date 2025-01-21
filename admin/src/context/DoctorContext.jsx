9e28170cd2e8b563762ae4aa720bbfc053f2e3c3
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-21 16:07:40 +0530

    Adding database cluster link

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 const DoctorContextProvider = (props) => {
   // const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const backendUrl = 'https://capstone-project-upgrad.onrender.com';
+  const backendUrl = 'https://capstone-project-upgrad-backend.onrender.com';
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
 import userRouter from './routes/userRoute.js';
 
-//app config
+// app config
 const app = express();
 const port = process.env.PORT || 3000;
 connectDB();
 connectCloudinary();
 
-//middlewares
-app.use(cors());
+// middlewares
+app.use(cors({
+  origin: 'https://legendary-banoffee-f0f5d4.netlify.app', // Your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true // Allow cookies or authentication if needed
+}));
 app.use(express.json());
 
-//api endpoints
+// api endpoints
 app.use('/api/admin', adminRouter);
-app.use('/api/doctor', doctorRouter)
-app.use('/api/user', userRouter)
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 
+app.get('/', (req, res) => {
+  res.send("API working");
+});
 
-app.get('/',(req, res) =>{
-    res.send("Api working")
-})
-
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`)
-})
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
