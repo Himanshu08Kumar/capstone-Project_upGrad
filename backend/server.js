0f04a82e21922cf63dfb494a283768aff6d839ee
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-21 19:13:15 +0530

    solving  CORS error1

--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,21 @@ connectCloudinary();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://ubiquitous-sprite-b4a2e0.netlify.app/', 
+    origin: 'https://ubiquitous-sprite-b4a2e0.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, // Allowed HTTP methods
 }));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://legendary-banoffee-f0f5d4.netlify.app");
+    res.header("Access-Control-Allow-Origin", "https://ubiquitous-sprite-b4a2e0.netlify.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
 app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://legendary-banoffee-f0f5d4.netlify.app");
+    res.header("Access-Control-Allow-Origin", "https://ubiquitous-sprite-b4a2e0.netlify.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.sendStatus(200);
