0819b7d5e02a5058eb212b31e56174d0ebd6e3a4
Himanshu Kumar <110741453+Himanshu08Kumar@users.noreply.github.com>
2025-01-21 12:26:47 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false)
 
-  const backendUrl = "https://capstone-project-upgrad.onrender.com";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const getAllDoctors = async () => {
     try {
