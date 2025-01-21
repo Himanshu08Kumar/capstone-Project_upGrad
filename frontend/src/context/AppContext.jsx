f9fa5c06b3baa63e8639434648ead63e6f85edf5
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-21 15:53:20 +0530

    Adding database cluster link

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,6 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
+  // const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const backendUrl = 'https://capstone-project-upgrad.onrender.com';
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
