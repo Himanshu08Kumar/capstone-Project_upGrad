516a2a1cf05cc517f6678a3804c21ad7fb2d1fd8
Himanshu Kumar <himanshukumar802123@gmail.com>
2025-01-25 23:41:53 +0530

    changing admin's URL1

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-import mongoose from "mongoose";
-
 const doctorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
